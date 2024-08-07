ce1d3b59f03e78f976629d3000dd0a22906e3915
Dishantmakvana <dishant.makvana2012@gmail.com>
2024-08-07 16:23:41 +0530

    product page
    product page

---
+++ script/items.js
@@ -0,0 +1,86 @@
+const items = [
+
+  {
+    id: '001',
+    item_image: './assets/images/cake/ricket-field-semi-fondant-birthday-cake.webp',
+    item_name: 'Delicious Cricket Field Cake',
+    current_price: 'Rs 999',
+    original_price: `Rs 1999`,
+    item_discount: '50',
+  },
+
+  {
+    id: '002',
+    item_image: './assets/images/cake/Anniversary Surprise Chocolate Pinata Ball Cake.webp',
+    item_name: 'Chocolate Pinata Ball Cake',
+    current_price: 'Rs 1499',
+    original_price: `Rs 2999`,
+    item_discount: '50',
+  },
+
+  {
+    id: '003',
+    item_image: './assets/images/cake/Decadent Dark Chocolate Cake.webp',
+    item_name: 'Decadent Dark Chocolate Cake',
+    current_price: 'Rs 999',
+    original_price: `Rs 1999`,
+    item_discount: '50',
+  },
+
+  {
+    id: '004',
+    item_image: './assets/images/cake/Lion Face Cake.webp',
+    item_name: 'Delicious Lion Face Cake',
+    current_price: 'Rs 999',
+    original_price: `Rs 1999`,
+    item_discount: '50',
+  },
+
+  {
+    id: '005',
+    item_image: './assets/images/cake/Tropical Fruit Cake.avif',
+    item_name: 'Delightful  Delicious fruit Cake',
+    current_price: 'Rs 1499',
+    original_price: `Rs 2999`,
+    item_discount: '50',
+  },
+
+  {
+    id: '006',
+    item_image: './assets/images/cake/black-and-white-teddy-bear-cake.webp',
+    item_name: 'black & white teddy bear cake',
+    current_price: 'Rs 999',
+    original_price: `Rs 1999`,
+    item_discount: '50',
+  },
+
+  {
+    id: '007',
+    item_image: './assets/images/cake/hazelnut-crunch-cake.webp',
+    item_name: 'hazelnut - crunch  - cake',
+    current_price: 'Rs 999',
+    original_price: `Rs 1999`,
+    item_discount: '50',
+  },
+
+  {
+    id: '008',
+    item_image: './assets/images/cake/jack-daniels-chocolate-cake.webp',
+    item_name: 'jack daniels chocolate  cake',
+    current_price: 'Rs 999',
+    original_price: `Rs 1999`,
+    item_discount: '50',
+  },
+  
+  {
+    id: '009',
+    item_image: './assets/images/cake/Delicious Rasmalai Cake-2.webp',
+    item_name: 'Delicious Rasmalai Cake',
+    current_price: 'Rs 999',
+    original_price: `Rs 1999`,
+    item_discount: '50',
+  },
+
+
+
+];
